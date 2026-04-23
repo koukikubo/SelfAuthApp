571af4e8b9724ea748de7a8eb9062112b0c537f8
koukikubo <kouki.xx.0817@gmail.com>
2026-04-23 19:16:04 +0900

    ✨ セッション情報を取得するAPIエンドポイントを実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "admin/login", to: "admin_sessions#create"
       post "staff/login", to: "staff_sessions#create"
-
+      get "session", to: "sessions#show"
       delete "logout", to: "sessions#destroy"
-      
+
       get "csrf", to: "csrf#show" 
     end
   end
