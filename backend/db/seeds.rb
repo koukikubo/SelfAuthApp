854109b7fca417c7ae628beece60b12c1800be8f
koukikubo <kouki.xx.0817@gmail.com>
2026-04-23 17:04:44 +0900

    ✨ データベースシードファイルに管理者とスタッフのレコードを追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 管理者
+Admin.create!(
+  name: "管理者",
+  password: "password",
+  password_confirmation: "password",
+  effective_from: Date.current
+)
+
+# スタッフ
+Staff.create!(
+  name: "大阪太郎",
+  password: "password",
+  password_confirmation: "password",
+  effective_from: Date.current,
+  role: :operator
+)
+
+Staff.create!(
+  name: "大阪花子",
+  password: "password",
+  password_confirmation: "password",
+  effective_from: Date.current,
+  role: :viewer
+)
