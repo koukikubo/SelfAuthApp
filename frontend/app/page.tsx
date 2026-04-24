f40502cf245bc9a542630cd51f429832c815be7e
koukikubo <kouki.xx.0817@gmail.com>
2026-04-24 23:40:51 +0900

    ♻️ ヘルスチェックからTOPページに変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,8 @@
 export default async function Page() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/health`,
-    {
-      cache: "no-store",
-    },
-  );
-
-  const data = await res.json();
-
   return (
     <main>
-      <h1>疎通確認</h1>
-      <p>{data.status}</p>
+      <h1>TOPページ</h1>
+      <p>ようこそ！</p>
     </main>
   );
 }
