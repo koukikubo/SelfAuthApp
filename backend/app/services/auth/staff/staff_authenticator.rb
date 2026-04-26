d8ffee003d2db2e8a5345178705aca5b9226d5dd
koukikubo <kouki.xx.0817@gmail.com>
2026-04-26 17:06:49 +0900

    ✨ スタッフ認証メソッドを簡素化し、不要な条件を削除

--- backend/app/services/auth/staff/staff_authenticator.rb
+++ backend/app/services/auth/staff/staff_authenticator.rb
@@ -9,19 +9,7 @@ module Auth
     # スタッフを認証する
       def authenticate
         staff = ::Staff.find_by(id: @id)
-        return nil unless staff
         authenticate_user(staff, @password)
-
-        return nil unless staff.active_for_login?
-        # パスワードが正しいか確認
-        if staff.authenticate(@password)
-          staff.reset_failed_attempts!
-          staff
-        else
-          # パスワードが間違っている場合は失敗回数を記録
-          staff.register_failed_attempt!
-          nil
-        end
       end
     end
   end
